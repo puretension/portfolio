de8610bbdf0fb0d05af845bebb2f091b8b8810fa
puretension <rlrlfhtm5@gmail.com>
2024-01-25 05:41:33 +0900

    feat: Home 화면 완성

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,25 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Layout from '../components/layout'
+import Layout from '../components/layout';
+import Hero from '../components/home/hero';
+import Animation from '../components/home/animation';
 
 //pages폴더는 실제 사이트에 뜰 View를 담당하는 폴더
 
 export default function Home() {
   return (
     <Layout>
-
       <Head>
         <title>puretension's portfolio</title>
         <meta name="description" content= "just me" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
-      {/* <h1>HOME!!</h1> */}
-      <h1 className="text-3xl font-bold underline">
-        안녕하세요!
-    </h1>
-      
+      <section className="flex min-h-screen flex col items-center justify-center text-gray-600 body-font">
+                <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+                      <Hero />
+                      <Animation />
+                </div>
+     </section>
     </Layout>
   )
 }
