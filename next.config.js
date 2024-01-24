4e61facf831f1cde10c46cb404c3204f627dcddf
puretension <rlrlfhtm5@gmail.com>
2024-01-25 05:42:53 +0900

    chore: 이미지를 위한 confing 수정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'images.unsplash.com', 
+      'www.notion.so',
+      's3.us-west-2.amazonaws.com',
+      'prod-files-secure.s3.us-west-2.amazonaws.com'
+    ],
+  },
 }
 
 module.exports = nextConfig
