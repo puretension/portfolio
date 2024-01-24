3bd894769babf38cbe93c150b639aa2f54b755ac
puretension <rlrlfhtm5@gmail.com>
2024-01-24 19:02:31 +0900

    feat: lottie 생성

---
+++ components/home/animation.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Lottie from 'react-lottie-player'
+// Alternatively:
+// import Lottie from 'react-lottie-player/dist/LottiePlayerLight'
+
+import lottieJson from '/public/animation.json'
+
+export default function Animation() {
+  return (
+    <Lottie
+      loop
+      animationData={lottieJson} // 여기에 들어가는거! 
+      play
+      style={{ width: 700, height: 700 }}
+    />
+  )
+}
