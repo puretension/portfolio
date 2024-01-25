3775aa25f136cf032fe1758a8ebe020643845bd6
puretension <rlrlfhtm5@gmail.com>
2024-01-25 17:27:05 +0900

    Design: Header 맥북 💻  추가

--- components/header.js
+++ components/header.js
@@ -8,9 +8,19 @@ export default function Header() {
             <header className="text-gray-600 body-font">
                 <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                     <Link href="/" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-                            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                        </svg>
+                        {/* 이미지 컨테이너 */}
+                        <div className="macbook-container">
+                            <img 
+                                src="/icons/mac.png" 
+                                alt="Mac Closed" 
+                                className="mac-closed"
+                            />
+                            <img 
+                                src="/icons/mac2.png" 
+                                alt="Mac Open" 
+                                className="mac-open"
+                            />
+                        </div>
                         <span className="ml-3 text-xl">DOHYEONG LEE</span>
                     </Link>
                     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
